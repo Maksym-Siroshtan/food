1d21a1202ab893e963b0091193c4bcf3f78836d3
Maksym Siroshtan <siroshtan.max1991UA@gmail.com>
2023-02-11 20:04:11 +0200

    Added js services

---
+++ js/services/services.js
@@ -0,0 +1,24 @@
+const postData = async (url, data) => {
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: data,
+  });
+
+  return await res.json();
+};
+
+//Получение данных с json-server с помощью promise(async, await). За основу взяты классы
+const getResourse = async (url) => {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, status ${res.status}`);
+  }
+
+  return await res.json();
+};
+
+export { postData, getResourse };
